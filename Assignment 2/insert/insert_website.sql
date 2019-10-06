b2a69f42947d230bede54c81b97824802e4d4347
QuanweiHao <45153945+QuanweiHao@users.noreply.github.com>
2019-10-06 11:17:20 -0400

    Add files via upload

---
+++ Assignment 2/insert/insert_website.sql
@@ -0,0 +1,312 @@
+INSERT INTO `assignment-2`.`website`
+(`id`,
+`name`,
+`description`,
+`created`,
+`updated`,
+`visits`,
+`developer`)
+VALUES
+(123,
+'Facebook',
+'an onlinne social media and social networking service',
+curdate(),
+curdate(),
+'1234234',
+12);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'owner',
+123,
+12);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'editor',
+123,
+23);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'admin',
+123,
+34);
+
+INSERT INTO `assignment-2`.`website`
+(`id`,
+`name`,
+`description`,
+`created`,
+`updated`,
+`visits`,
+`developer`)
+VALUES
+(234,
+'Twitter',
+'an online news and social networking service',
+curdate(),
+curdate(),
+'4321543',
+23);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'owner',
+234,
+23);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'editor',
+234,
+34);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'admin',
+234,
+12);
+
+INSERT INTO `assignment-2`.`website`
+(`id`,
+`name`,
+`description`,
+`created`,
+`updated`,
+`visits`,
+`developer`)
+VALUES
+(345,
+'Wikipedia',
+'a free online encyclopedia',
+curdate(),
+curdate(),
+'345654',
+34);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'owner',
+345,
+34);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'editor',
+345,
+12);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'admin',
+345,
+23);
+
+INSERT INTO `assignment-2`.`website`
+(`id`,
+`name`,
+`description`,
+`created`,
+`updated`,
+`visits`,
+`developer`)
+VALUES
+(456,
+'CNN',
+'an American basic cable and satellite television news channel',
+curdate(),
+curdate(),
+'654345',
+12);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'owner',
+456,
+12);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'editor',
+456,
+23);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'admin',
+456,
+34);
+
+INSERT INTO `assignment-2`.`website`
+(`id`,
+`name`,
+`description`,
+`created`,
+`updated`,
+`visits`,
+`developer`)
+VALUES
+(567,
+'CNET',
+'an American media website that publishes reviews, news, articles, blogs, podcasts and videos on technology annd consumer electronics',
+curdate(),
+curdate(),
+'5433455',
+23);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'owner',
+567,
+23);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'editor',
+567,
+34);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'admin',
+567,
+12);
+
+INSERT INTO `assignment-2`.`website`
+(`id`,
+`name`,
+`description`,
+`created`,
+`updated`,
+`visits`,
+`developer`)
+VALUES
+(678,
+'Gizmodo',
+'a design, technology, science and science fiction website that also write articles on politics',
+curdate(),
+curdate(),
+'432345',
+34);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'owner',
+678,
+34);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'editor',
+678,
+12);
+
+INSERT INTO `assignment-2`.`website_role`
+(`id`,
+`role`,
+`website`,
+`developer`)
+VALUES
+(null,
+'admin',
+678,
+23);
+
+
+
+
+
+
+
+
+
+
+
+
+
