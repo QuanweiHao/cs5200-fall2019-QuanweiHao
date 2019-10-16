b3c80d24991f65a7295835a40ad93334d7edcc36
QuanweiHao <45153945+QuanweiHao@users.noreply.github.com>
2019-10-16 11:46:08 -0400

    Delete create_user.sql

--- Assignment 2/create/create_user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `user_aggrement` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `user_person_generalization` FOREIGN KEY (`id`) REFERENCES `person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-)
