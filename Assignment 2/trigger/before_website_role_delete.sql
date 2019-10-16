0dfe7783236061d8555aea03b63c2c13c1801b2c
QuanweiHao <45153945+QuanweiHao@users.noreply.github.com>
2019-10-16 12:29:01 -0400

    Delete before_website_role_delete.sql

--- Assignment 2/trigger/before_website_role_delete.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`quanwei`@`%` TRIGGER `assignment-2`.`website_role_BEFORE_DELETE` BEFORE DELETE ON `website_role` FOR EACH ROW
-BEGIN
-	delete from website_privilege
-    where developer = old.developer and website = old.website;
-END
