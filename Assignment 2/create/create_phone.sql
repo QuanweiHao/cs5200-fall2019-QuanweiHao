32b662a6844c71113459d4bcd9e6a426f959b6fa
QuanweiHao <45153945+QuanweiHao@users.noreply.github.com>
2019-10-16 11:45:08 -0400

    Delete create_phone.sql

--- Assignment 2/create/create_phone.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `phone` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(45) DEFAULT NULL,
-  `primary` tinyint(4) DEFAULT NULL,
-  `person` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `phone_person_composition_idx` (`person`),
-  CONSTRAINT `phone_person_composition` FOREIGN KEY (`person`) REFERENCES `person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) 
