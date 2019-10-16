0d8a7e76dd59dc541efe45f156d15bfb8d57488e
QuanweiHao <45153945+QuanweiHao@users.noreply.github.com>
2019-10-16 11:44:30 -0400

    Delete create_developer.sql

--- Assignment 2/create/create_developer.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `developer` (
-  `id` int(11) NOT NULL,
-  `developer_key` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `developer_person_generalization` FOREIGN KEY (`id`) REFERENCES `person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-)
